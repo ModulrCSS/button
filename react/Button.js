600b4e3c2d24ce1db4532b681c577cc3079afb4e
uloga <wpnemesis@gmail.com>
2017-07-01 15:44:55 +0200

    react test

---
+++ react/Button.js
@@ -0,0 +1,35 @@
+// this is just for testing of loading as npm module
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Button.css';
+
+class ModButton extends Component {
+
+  constructor(props) {
+    
+    super(props);
+
+    this.state = {
+      msg: 'this is a message'
+    };
+    
+    this.update = this.update.bind(this);
+
+  }
+
+  update(e){
+    this.setState({
+      msg: 'this s a message update'
+    });
+  }
+
+  render(){
+    return(
+      <button className={styles.button} onClick={this.update}>this.props.children</button>
+    );
+  }
+
+}
+
+export default ModButton;
